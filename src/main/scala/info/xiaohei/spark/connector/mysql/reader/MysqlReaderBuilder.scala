62609faecb5613dcb7c52b2ffa4c003b1286f4f8
xiaohei <yuande.jiang@fugetech.com>
2017-03-26 21:21:30 +0800

    feature: added where conditions

---
+++ src/main/scala/info/xiaohei/spark/connector/mysql/reader/MysqlReaderBuilder.scala
@@ -0,0 +1,11 @@
+package info.xiaohei.spark.connector.mysql.reader
+
+/**
+  * Author: xiaohei
+  * Date: 2017/3/26
+  * Email: yuande.jiang@fugetech.com
+  * Host: xiaohei.info
+  */
+class MysqlReaderBuilder {
+
+}
--- src/main/scala/info/xiaohei/spark/connector/mysql/writer/MysqlWriterBuilder.scala
+++ src/main/scala/info/xiaohei/spark/connector/mysql/writer/MysqlWriterBuilder.scala
@@ -8,17 +8,19 @@ import java.sql.{DriverManager, PreparedStatement}
   * Email: yuande.jiang@fugetech.com
   * Host: xiaohei.info
   */
-case class MysqlWriterBuilder[C](
-                                            //todo:从conf中读取
-                                            private[mysql] val connectStr: String,
-                                            private[mysql] val username: String,
-                                            private[mysql] val password: String,
-                                            private[mysql] val collectionData: Iterable[C],
-                                            //todo:t.productIterator.foreach{ i =>println("Value = " + i )}
-                                            private[mysql] val fitStatement: (PreparedStatement, C) => Unit,
-                                            private[mysql] val columns: Iterable[String] = Seq.empty,
-                                            private[mysql] val tableName: Option[String] = None
-                                          ) {
+case class MysqlWriterBuilder[C] private[mysql](
+                                                 //todo:从conf中读取
+                                                 private[mysql] val connectStr: String,
+                                                 private[mysql] val username: String,
+                                                 private[mysql] val password: String,
+                                                 private[mysql] val collectionData: Iterable[C],
+                                                 //todo:t.productIterator.foreach{ i =>println("Value = " + i )}
+                                                 private[mysql] val fitStatement: (PreparedStatement, C) => Unit,
+                                                 private[mysql] val columns: Iterable[String] = Seq.empty,
+                                                 private[mysql] val tableName: Option[String] = None,
+                                                 //todo:完善
+                                                 private[mysql] val whereConditions: Option[String] = None
+                                               ) {
   def insert(cols: String*) = {
     require(this.columns.isEmpty, "Columns haven't been set")
     require(cols.nonEmpty, "Columns must by set,at least one")
@@ -33,7 +35,9 @@ case class MysqlWriterBuilder[C](
     this.copy(tableName = Some(table))
   }
 
-  //todo:where
+  def where(conditions: String) = {
+    this.copy(whereConditions = Some(conditions))
+  }
 }
 
 private[mysql] class MysqlWriterBuildMaker[C](collectionData: Iterable[C]) extends Serializable {
@@ -52,8 +56,11 @@ private[mysql] class MysqlWriter[C](builder: MysqlWriterBuilder[C])
     var placeholder = ""
     //todo:改进
     for (i <- 0 until builder.columns.size) placeholder += "?,"
-    val sql = s"insert into ${builder.tableName}(${builder.columns.mkString(",")}) values(${placeholder.substring(0, placeholder.length)})"
+    var sql = s"insert into ${builder.tableName}(${builder.columns.mkString(",")}) values(${placeholder.substring(0, placeholder.length)})"
     println(sql)
+    if (builder.whereConditions.nonEmpty) {
+      sql += s" where ${builder.whereConditions}"
+    }
     val ps = conn.prepareStatement(sql)
     Class.forName("com.mysql.jdbc.Driver")
     try {
