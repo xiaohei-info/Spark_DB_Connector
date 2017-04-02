87596431f974f1b283946c771e874fdb4f6045ab
xiaohei <yuande.jiang@fugetech.com>
2017-04-02 18:24:21 +0800

    feature: add columns method for case class

--- src/main/scala/info/xiaohei/spark/connector/hbase/transformer/DataTransformer.scala
+++ src/main/scala/info/xiaohei/spark/connector/hbase/transformer/DataTransformer.scala
@@ -6,6 +6,9 @@ package info.xiaohei.spark.connector.hbase.transformer
   * Email: xiaohei.info@gmail.com
   * Host: www.xiaohei.info
   */
-trait DataTransformer extends Serializable{
+trait DataTransformer extends Serializable {
   type HBaseData = Iterable[Option[Array[Byte]]]
+
+  //自定义class class时指定列名使用
+  def columns: Iterable[String] = Iterable.empty
 }
