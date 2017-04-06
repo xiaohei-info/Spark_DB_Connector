69e4b6920e65d2debb74a19435e4b6db97a5c940
xiaohei <yuande.jiang@fugetech.com>
2017-04-06 14:15:11 +0800

    extend with conversions

--- src/main/scala/info/xiaohei/spark/connector/mysql/package.scala
+++ src/main/scala/info/xiaohei/spark/connector/mysql/package.scala
@@ -1,9 +1,13 @@
 package info.xiaohei.spark.connector
 
+import info.xiaohei.spark.connector.mysql.reader.MysqlReaderBuilderConversions
+import info.xiaohei.spark.connector.mysql.writer.MysqlWriterBuilderConversions
+
 /**
   * Author: xiaohei
   * Date: 2017/4/6
   * Email: yuande.jiang@fugetech.com
   * Host: xiaohei.info
   */
-package object mysql extends MysqlConfConversions
+package object mysql extends MysqlReaderBuilderConversions
+  with MysqlWriterBuilderConversions
