5d80ec1e343f54c9b541b2c16bfce0219e25cb0f
xiaohei <yuande.jiang@fugetech.com>
2017-04-07 11:50:19 +0800

    add implicit conversion of mysql context

--- src/main/scala/info/xiaohei/spark/connector/mysql/package.scala
+++ src/main/scala/info/xiaohei/spark/connector/mysql/package.scala
@@ -1,6 +1,6 @@
 package info.xiaohei.spark.connector
 
-import info.xiaohei.spark.connector.mysql.reader.MysqlReaderBuilderConversions
+import info.xiaohei.spark.connector.mysql.reader.{MysqlConextConversions, MysqlReaderBuilderConversions}
 import info.xiaohei.spark.connector.mysql.writer.MysqlWriterBuilderConversions
 
 /**
@@ -12,3 +12,4 @@ import info.xiaohei.spark.connector.mysql.writer.MysqlWriterBuilderConversions
 package object mysql extends MysqlReaderBuilderConversions
   with MysqlWriterBuilderConversions
   with MysqlConfConversions
+  with MysqlConextConversions
