b77e9546c0bfd34db306a6c4f0b018eac9177ac9
xiaohei <yuande.jiang@fugetech.com>
2017-04-10 11:42:44 +0800

    feature: add implicit function for relational db entry convert to mysql context

---
+++ src/main/scala/info/xiaohei/spark/connector/RelationalDbEntry.scala
@@ -0,0 +1,11 @@
+package info.xiaohei.spark.connector
+
+/**
+  * Author: xiaohei
+  * Date: 2017/4/10
+  * Email: yuande.jiang@fugetech.com
+  * Host: xiaohei.info
+  */
+class RelationalDbEntry {
+
+}
