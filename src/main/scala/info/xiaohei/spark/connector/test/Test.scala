ca7ec462746b115a3b606e7aaca940bd060a0dd7
xiaohei <yuande.jiang@fugetech.com>
2017-04-15 17:14:49 +0800

    test

--- src/main/scala/info/xiaohei/spark/connector/test/Test.scala
+++ src/main/scala/info/xiaohei/spark/connector/test/Test.scala
@@ -1,5 +1,8 @@
 package info.xiaohei.spark.connector.test
 
+import info.xiaohei.spark.connector.hbase._
+import info.xiaohei.spark.connector.mysql.MysqlConf
+
 /**
   * Author: xiaohei
   * Date: 2017/4/6
@@ -7,5 +10,21 @@ package info.xiaohei.spark.connector.test
   * Host: xiaohei.info
   */
 object Test {
+  def main(args: Array[String]) {
 
+    implicit val mysqlConf = MysqlConf.createConf(
+      "your-host",
+      "username",
+      "password",
+      "port",
+      "db-name"
+    )
+    implicit val hBaseConf = HBaseConf.createConf("localhost")
+
+    val list = Seq[String]("")
+    list.toHBase("")
+      .insert("")
+      .inColumnFamily("")
+      .save()
+  }
 }
