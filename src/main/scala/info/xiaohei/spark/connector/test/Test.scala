21a00bd2f0722e9b7ef6102bb64a70561e264582
xiaohei <yuande.jiang@fugetech.com>
2017-04-17 10:36:07 +0800

    closed #9

--- src/main/scala/info/xiaohei/spark/connector/test/Test.scala
+++ src/main/scala/info/xiaohei/spark/connector/test/Test.scala
@@ -2,6 +2,8 @@ package info.xiaohei.spark.connector.test
 
 import info.xiaohei.spark.connector.RelationalDbEntry
 import info.xiaohei.spark.connector.mysql._
+import info.xiaohei.spark.connector.mysql.transformer.executor.{CustomDataExecutor, DataExecutor}
+import info.xiaohei.spark.connector.mysql.transformer.mapper.{CustomDataMapper, DataMapper}
 
 /**
   * Author: xiaohei
@@ -9,6 +11,9 @@ import info.xiaohei.spark.connector.mysql._
   * Email: yuande.jiang@fugetech.com
   * Host: xiaohei.info
   */
+
+case class Model(id: Int, name: String, age: Int)
+
 object Test {
   def main(args: Array[String]) {
 
@@ -20,10 +25,17 @@ object Test {
       "its"
     )
     val entry = new RelationalDbEntry
-    val res = entry.fromMysql[(Int, String, Int)]("test")
+    val res = entry.fromMysql[Model]("test")
       .select("id", "name", "age")
       .get
-    res.foreach(x => println(s"id:${x._1},name:${x._2},age:${x._3}"))
+    res.foreach(x => println(s"id:${x.id},name:${x.name},age:${x.age}"))
+  }
 
+  implicit def myExecutorConversion: DataExecutor[Model] = new CustomDataExecutor[Model, (Int, String, Int)]() {
+    override def convert(data: Model): (Int, String, Int) = (data.id, data.name, data.age)
+  }
+
+  implicit def myMapperConversion: DataMapper[Model] = new CustomDataMapper[(Int, String, Int), Model]() {
+    override def convert(data: (Int, String, Int)): Model = Model(data._1, data._2, data._3)
   }
 }
