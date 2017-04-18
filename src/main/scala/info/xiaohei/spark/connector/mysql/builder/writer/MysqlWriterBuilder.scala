70aa7ca5b4b8b6e7ba05a24676355d05517f63f1
xiaohei <yuande.jiang@fugetech.com>
2017-04-18 12:18:28 +0800

    style: make condition of columnNames into one line.resolved #3

--- src/main/scala/info/xiaohei/spark/connector/mysql/builder/writer/MysqlWriterBuilder.scala
+++ src/main/scala/info/xiaohei/spark/connector/mysql/builder/writer/MysqlWriterBuilder.scala
@@ -43,12 +43,8 @@ private[mysql] class MysqlWriter[C](builder: MysqlWriterBuilder[C])(implicit mys
     val (connectStr, username, password) = mysqlConf.getMysqlInfo()
     val conn = DriverManager.getConnection(connectStr, username, password)
 
+    val columnNames = if (builder.columns.toList.contains("*")) "" else s"(${builder.columns.mkString(",")})"
     var placeholder = ""
-    val columnNames =
-      if (builder.columns.toList.contains("*")) ""
-      else {
-        s"(${builder.columns.mkString(",")})"
-      }
     for (i <- 0 until builder.columns.size) placeholder += "?,"
     var sql = s"insert into ${builder.tableName}$columnNames values(${placeholder.substring(0, placeholder.length - 1)})"
     if (builder.whereConditions.nonEmpty) {
