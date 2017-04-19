0c30a958aad6319a8cbff62648faa2828fd180f6
xiaohei <yuande.jiang@fugetech.com>
2017-04-19 12:23:26 +0800

    feature: add verify fucntion for producer to check salt length

--- src/main/scala/info/xiaohei/spark/connector/hbase/salt/SaltProducer.scala
+++ src/main/scala/info/xiaohei/spark/connector/hbase/salt/SaltProducer.scala
@@ -16,8 +16,16 @@ abstract class SaltProducer[T](saltArray: Iterable[T]) extends Serializable {
     require(writer.isInstanceOf[SingleColumnDataWriter[T]], "salt array must be composed with primitive type")
 
     val singleColumnDataWriter = writer.asInstanceOf[SingleColumnDataWriter[T]]
-    saltArray.map(s => singleColumnDataWriter.writeSingleColumn(s))
+    val saltLength = saltArray.map(s => singleColumnDataWriter.writeSingleColumn(s))
       .map(b => b.getOrElse(Array[Byte]()))
       .map(_.length)
+      //todo:优化
+      .foldLeft(None.asInstanceOf[Option[Int]])((size1, size2) => {
+      if (size1.nonEmpty && size1.get != size2) {
+        throw new IllegalArgumentException(s"salts can not use different lengths with:${size1.get},$size2")
+      }
+      Some(size2)
+    }).get
+    require(saltLength > 0, "salt's length must great than 0")
   }
 }
