a1e9a7a085f31f447ffc2920c7bd5a61e4eb53a1
xiaohei <yuande.jiang@fugetech.com>
2017-04-25 11:59:16 +0800

    feature: add salt rdd that can union ranges

---
+++ src/main/scala/info/xiaohei/spark/connector/hbase/builder/reader/HBaseSaltRDD.scala
@@ -0,0 +1,16 @@
+package info.xiaohei.spark.connector.hbase.builder.reader
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.{RDD, UnionRDD}
+
+import scala.reflect.ClassTag
+
+/**
+  * Author: xiaohei
+  * Date: 2017/4/25
+  * Email: yuande.jiang@fugetech.com
+  * Host: xiaohei.info
+  */
+class HBaseSaltRDD[R: ClassTag](sc: SparkContext, rdds: Seq[RDD[R]]) extends UnionRDD[R](sc, rdds) {
+
+}
