c314937d37cca0d35467cf867d56c3be8a6674ba
xiaohei <yuande.jiang@fugetech.com>
2017-04-25 10:47:04 +0800

    refactor: split verifyLength to verify and length

--- src/main/scala/info/xiaohei/spark/connector/hbase/salt/SaltProducer.scala
+++ src/main/scala/info/xiaohei/spark/connector/hbase/salt/SaltProducer.scala
@@ -17,26 +17,29 @@ trait SaltProducer[T] extends Serializable {
 
   protected def salts: Array[T]
 
-  protected def verifySaltLength(implicit writer: DataWriter[T]): Unit = {
+  protected def verify(implicit writer: DataWriter[T]): Unit = {
+    require(length > 0, "salt's length must great than 0")
+  }
+
+  def length(implicit writer: DataWriter[T]): Int = {
     require(writer.isInstanceOf[SingleColumnDataWriter[T]], "salt array must be composed with primitive type")
 
     val singleColumnDataWriter = writer.asInstanceOf[SingleColumnDataWriter[T]]
-    val saltLength = salts.map(s => singleColumnDataWriter.writeSingleColumn(s))
+    salts.map(s => singleColumnDataWriter.writeSingleColumn(s))
       .map(b => b.getOrElse(Array[Byte]()))
       .map(_.length)
       .foldLeft(None.asInstanceOf[Option[Int]])((size1, size2) => {
-      if (size1.nonEmpty && size1.get != size2) {
-        throw new IllegalArgumentException(s"salts can not use different lengths with:${size1.get},$size2")
-      }
-      Some(size2)
-    }).get
-    require(saltLength > 0, "salt's length must great than 0")
+        if (size1.nonEmpty && size1.get != size2) {
+          throw new IllegalArgumentException(s"salts can not use different lengths with:${size1.get},$size2")
+        }
+        Some(size2)
+      }).get
   }
 }
 
 private[salt] class RandomSaltProducer[T: ClassTag](val salts: Array[T])(implicit writer: DataWriter[T]) extends SaltProducer[T]() {
 
-  verifySaltLength
+  verify
 
   override def salting(rowkey: Array[Byte]): T = {
     val randomizer = new Random
@@ -46,7 +49,7 @@ private[salt] class RandomSaltProducer[T: ClassTag](val salts: Array[T])(implici
 
 private[salt] class HashSaltProducer[T: ClassTag](val salts: Array[T])(implicit writer: DataWriter[T]) extends SaltProducer[T]() {
 
-  verifySaltLength
+  verify
 
   override def salting(rowkey: Array[Byte]): T = {
     salts((java.util.Arrays.hashCode(rowkey) & 0x7fffffff) % salts.length)
